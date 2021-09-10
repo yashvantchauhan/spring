3669238752564130590ad804ae66aa090998bcbf
yashvantchauhan <yashvant.chauhan@gmail.com>
2021-09-10 23:13:41 +0530

    "Updated for image"

--- reactive/src/main/java/com/example/reactive/controller/HelloController.java
+++ reactive/src/main/java/com/example/reactive/controller/HelloController.java
@@ -1,15 +1,28 @@
 package com.example.reactive.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/reactive")
+@Slf4j
 public class HelloController {
+
+    @Value("${example.datasource.url}")
+    String url;
+
+    @Value("${example.datasource.username}")
+    String username;
+    @Value("${example.datasource.password}")
+    String password;
 
     @GetMapping
     public String hello(){
+
+        log.info("URL : {}, username: {}, password : {}", url, username, password );
         return "Hello there!";
     }
 }
