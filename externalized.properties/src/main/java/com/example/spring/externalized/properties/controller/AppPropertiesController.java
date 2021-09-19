a2c858ee39f37a1e8f4e2a721e1d06a2d3519d5a
yashvantchauhan <yashvant.chauhan@gmail.com>
2021-09-19 21:45:03 +0530

    Spring boot externalized application configuration

--- externalized.properties/src/main/java/com/example/spring/externalized/properties/controller/AppPropertiesController.java
+++ externalized.properties/src/main/java/com/example/spring/externalized/properties/controller/AppPropertiesController.java
@@ -1,24 +1,42 @@
 package com.example.spring.externalized.properties.controller;
 
 import com.example.spring.externalized.properties.config.TenantConfig;
+import com.example.spring.externalized.properties.model.ConfigResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/properties")
 @Slf4j
 public class AppPropertiesController {
 
+    @Value("${datasource.username}")
+    String username;
+
+    @Value("${datasource.password}")
+    String password;
 
     @Autowired
     TenantConfig tenantConfig;
 
-    @GetMapping("{key}")
-    public Mono<String> getPropertiesValue(@PathVariable String key){
+    @GetMapping(value = "{key}", produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public Mono<ConfigResponse> getPropertiesValue(@PathVariable String key){
+
+        Map config= new HashMap();
+        config.put(key, tenantConfig.getTenant().get(key));
         log.info("Received key: {} , Value: {} ",key, tenantConfig.getTenant().get(key));
-        return Mono.just(String.format("Key: %s, value: %s", key, tenantConfig.getTenant().get(key)));
+
+        ConfigResponse response=ConfigResponse.builder().
+                config(config).databasePassword(password).databaseUsername(username).build();
+        log.info("Response: {} ",response);
+        return Mono.just(response);
     }
 }
---
+++ externalized.properties/src/main/java/com/example/spring/externalized/properties/model/ConfigResponse.java
@@ -0,0 +1,16 @@
+package com.example.spring.externalized.properties.model;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
+
+import java.util.Map;
+
+@Data
+@Builder
+@ToString
+public class ConfigResponse {
+    String databaseUsername;
+    String databasePassword;
+    Map<String, String> config;
+}
